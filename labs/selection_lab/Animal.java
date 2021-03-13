b6b31b51e0f4ee532c49d91682bff6557cd1687d
jesus <jvillalba23@yahoo.com>
2021-03-13 16:05:51 -0600

    Updated learning repo, worked on sequence fundamental

---
+++ labs/selection_lab/Animal.java
@@ -0,0 +1,24 @@
+/*
+Local Variables
+Class Variables
+Instance Variables
+
+
+Class Scope
+Method Scope
+Block Scope
+*/
+
+public class Animal{
+    public String name;
+    // Constructor
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    // Helper methods
+    public String getName() {
+        return "Yo my name is... " + this.name;
+    }
+
+}
